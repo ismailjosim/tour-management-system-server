891b605e16e05fdeab82ede73ab2d5c08e014360
ismailjosim <ismailjosim99@gmail.com>
2025-07-17 20:44:04 +0600

    TourType and Tour interface and model done

---
+++ src/app/modules/division/division.interface.ts
@@ -0,0 +1,10 @@
+export interface IDivision {
+	name: string
+	slug: string
+	thumbnail?: string
+	description?: string
+}
+
+// name: Dhaka Division
+// slug: dhaka-division[name will be lowercase + add - between text]
+// what we do with slug: we use slug instead of :id it looks better rather then id
