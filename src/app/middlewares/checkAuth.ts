7b53d733cca7aa79fa3de85d5e572ce300f4d94a
ismailjosim <ismailjosim99@gmail.com>
2025-08-16 16:53:35 +0600

    fix: improve access token retrieval and enhance invoice template styling

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -11,7 +11,7 @@ const checkAuth =
 	(...authRoles: string[]) =>
 	async (req: Request, res: Response, next: NextFunction) => {
 		try {
-			const accessToken = req.headers.authorization
+			const accessToken = req.headers.authorization || req.cookies.accessToken
 
 			if (!accessToken) {
 				throw new AppError(httpStatus.BAD_REQUEST, 'Token not received')
